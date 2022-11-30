7fa1a69e126eef5faca2c97a51ae355b346145bf
ShivaKanchi <shivakanchi111@gmail.com>
2022-11-30 20:25:00 +0530

    Project done

--- dist/Config/google.config.js
+++ dist/Config/google.config.js
@@ -17,7 +17,7 @@ var _default = passport => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4000/auth/google/callback"
+    callbackURL: "https://zomato-clone-shiva.onrender.com/auth/google/callback"
   }, async (accessToken, refreshToken, profile, done) => {
     const newUser = {
       fullname: profile.displayName,
